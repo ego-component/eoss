c5c6707bac2062ed99eafdf9b8ccf0b78befaf13
clannadxr <clannadxr@hotmail.com>
2023-10-10 18:19:19 +0800

    copy Content-Encoding

--- aws.go
+++ aws.go
@@ -53,23 +53,24 @@ func (a *S3) Copy(srcKey, dstKey string, options ...CopyOption) error {
 	if cfg.metaKeysToCopy != nil || cfg.meta != nil {
 		input.SetMetadataDirective("REPLACE")
 		input.Metadata = make(map[string]*string)
-	}
-	// copy metadata
-	if len(cfg.metaKeysToCopy) > 0 {
-		// 如果传了 attributes 数组的情况下只做部分 meta 的拷贝
-		metadata, err := a.Head(srcKey, cfg.metaKeysToCopy)
+		cfg.metaKeysToCopy = append(cfg.metaKeysToCopy, "Content-Encoding") // always copy content-encoding
+		metadata, err := a.WithContext(a.ctx).Head(srcKey, cfg.metaKeysToCopy)
 		if err != nil {
 			return err
 		}
 		if len(metadata) > 0 {
 			for k, v := range metadata {
+				if k == "Content-Encoding" {
+					input.ContentEncoding = aws.String(v)
+					continue
+				}
 				input.Metadata[k] = aws.String(v)
 			}
 		}
-	}
-	// specify new metadata
-	for k, v := range cfg.meta {
-		input.Metadata[k] = aws.String(v)
+		// specify new metadata
+		for k, v := range cfg.meta {
+			input.Metadata[k] = aws.String(v)
+		}
 	}
 	_, err = a.Client.CopyObjectWithContext(a.ctx, input)
 	if err != nil {
